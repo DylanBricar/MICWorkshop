582e973d5313949ee885787fa7f83fa5c0b1576e
Dylan BRICAR <54027@etu.he2b.be>
2022-02-18 13:46:05 +0100

    Add socket IO and connect with backend

--- server-auth/src/Controller/User.js
+++ server-auth/src/Controller/User.js
@@ -1,45 +1,55 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const User = require('../Models/User')
+const User = require("../Models/User");
 
-router.get('/', async(req, res) => {
-    try {
-        const users = await User.find().select(["-_id", "-password"]);
-        res.send(users)
-    } catch (error) {
-        res.status(500).send({ error: error });
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find().select(["-_id", "-password"]);
+    res.send(users);
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+
+    const users = await User.find().select(["-_id", "-password"]);
+    IO.emit("new-user", users);
+
+    res.send(users);
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
+
+router.post("/connect", async (req, res) => {
+  try {
+    const user = await User.findOne(
+      {
+        name: req.body.name,
+        password: req.body.password,
+      },
+      "-password"
+    );
+
+    if (!user || !user._id) {
+      throw "Problem with user or password, try again.";
     }
-})
+    await user.updateOne({ connected: true });
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
 
-router.post('/', async(req, res) => {
-    try {
-        const user = new User(req.body)
-        await user.save()
+let IO = {};
+const setIO = (io) => {
+  IO = io;
+};
 
-        const users = await User.find().select(["-_id", "-password"]);
-        res.send(users);
-    } catch (error) {
-        res.status(500).send({ error: error });
-    }
-})
-
-router.post('/connect', async(req, res) => {
-    try {
-        const user = await User.findOne({
-            name: req.body.name,
-            password: req.body.password
-        }, "-password")
-
-        if (!user || !user._id) {
-            throw "Problem with user or password, try again."
-        }
-        await user.updateOne({ connected: true })
-        await user.save()
-        res.send(user)
-    } catch (error) {
-        res.status(500).send({ error: error });
-    }
-})
-
-module.exports = router
+module.exports = { router, setIO };
--- server-auth/src/index.js
+++ server-auth/src/index.js
@@ -1,25 +1,32 @@
 const express = require("express");
-const http = require('http');
-const cors = require("cors")
+const http = require("http");
+const cors = require("cors");
+const socketIO = require("socket.io");
 
 /** MongoDB database */
-const db = require("./DAL/mongodb")
+const db = require("./DAL/mongodb");
 
 const PORT = 4445;
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
+
+const socket = socketIO(server, {
+  transport: ["polling"],
+  cors: { cors: { origin: "*" } },
+});
 
 /** Models */
-const User = require("./Models/User")
+const User = require("./Models/User");
 
 /** Middleware */
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 /** Controllers */
-const userController = require("./Controller/User")
-app.use("/user", userController)
+const { router, setIO } = require("./Controller/User");
+setIO(socket);
+app.use("/user", router);
 
 server.listen(PORT, "0.0.0.0", () => {
-    console.log(`Serveur started ${PORT}`)
-})
+  console.log(`Serveur started ${PORT}`);
+});
